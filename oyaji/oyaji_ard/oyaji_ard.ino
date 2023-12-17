98305d4e011c6d8ac6b83f06be81b02c36037db9
1020228miurahayato <133774630+1020228miurahayato@users.noreply.github.com>
2023-12-17 10:45:16 +0900

    Update oyaji_ard.ino

--- oyaji/oyaji_ard/oyaji_ard.ino
+++ oyaji/oyaji_ard/oyaji_ard.ino
@@ -1,24 +1,23 @@
 #include <Servo.h>
-float raw_value;
-float weight;
+Servo servo; //サーボモータ
 
-Servo servo;
-//サーボ配線　茶：GND　赤：5V　黄：pin
+float raw_value;
+float weight; //かかる圧力
 
 void setup() { 
   Serial.begin(9600);
-  servo.attach(7,500,2400);
+  servo.attach(7,500,2400); //パルス幅を500~2400msに指定
   servo.write(111); //モータ初期角度決め
   
 }
 
 void loop() {
   raw_value = analogRead(A0);
-  weight = raw_value / 1024 * 200;　//かかる圧力 圧力が大きいほど数値が小さくなる
+  weight = raw_value / 1024 * 200;　//圧力が大きいほど数値が小さくなる
   Serial.println(weight);
 
   //圧力センサが反応したら顎動かす
-  if(weight <= 140){
+  if(weight <= 180){
     servo.write(45); //口開ける
     delay(1000); //1秒間口開ける
     servo.write(111); //もとに戻す
